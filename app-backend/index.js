0f5a7238c1648f4cabdd69e3c868c24dbdc6c2c8
Cubenode-ops <sa.info.mak@gmx.ch>
2026-05-11 16:07:13 +0200

    connect backend to postgres with docker compose

--- app-backend/index.js
+++ app-backend/index.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const { Pool } = require("pg");
+
 const app = express();
+
+const pool = new Pool({
+  user: "postgres",
+  host: "db",
+  database: "postgres",
+  password: "postgres",
+  port: 5432,
+});
 
 app.get("/", (req, res) => {
   res.send("Backend läuft 🚀");
+});
+
+app.get("/db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("DB Fehler");
+  }
 });
 
 app.listen(3000, () => {
