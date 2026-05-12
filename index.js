7ba9dee2fc77e548a990359cec3a6130eedc775e
Cubenode-ops <sa.info.mak@gmx.ch>
2026-05-12 03:08:15 +0200

    clean backend structure

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// routes
+const notesRoutes = require("./routes/notes");
+app.use("/notes", notesRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend läuft 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Backend läuft auf Port 3000");
+});
+
+
