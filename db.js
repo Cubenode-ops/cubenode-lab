b631cac9b8cc3e823e76b69fca7dc1404fd2e2d6
Cubenode-ops <sa.info.mak@gmx.ch>
2026-05-21 17:46:08 +0200

    Refactor backend to use environment variables and improve Docker compatibility

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "db",
-  database: "postgres",
-  password: "postgres",
-  port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
